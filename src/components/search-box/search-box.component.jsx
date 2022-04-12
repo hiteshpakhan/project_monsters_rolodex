d7978ea084af99789a077410134b94810feeead6
Hitesh Pakhan <hitesh5pakhan@gmail.com>
2022-04-12 10:19:57 +0530

    a

--- src/components/search-box/search-box.component.jsx
+++ src/components/search-box/search-box.component.jsx
@@ -4,7 +4,7 @@ import "./search-box.style.css";
 export const SearchBox = ({placeholder, handleChange}) => (
     <input 
         className="search"
-        type="search" 
+        type="search"
         placeholder= {placeholder}
         onChange={ handleChange }
     />
